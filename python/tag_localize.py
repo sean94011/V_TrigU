c108146343e256bc5f90e6e41a40293a987300af
Power-tile <hejizheng2004@163.com>
2023-02-16 15:40:28 -0600

    Tag localize initial commit

--- python/tag_localize.py
+++ python/tag_localize.py
@@ -21,8 +21,12 @@ time_vec = np.linspace(0,Ts*(Nfft-1),num=Nfft)
 dist_vec = time_vec*(c/2) # distance in meters
 
 rad = isens_vtrigU()
-cal_data, rec_data, _ = rad.load_data(case='tag/', scenario='test')
-data = rad.calibration(cal_data, rec_data)
+# cal_data, rec_data, _ = rad.load_data(case='tag/', scenario='switch10steps2m')
+cal_data, rec_data, _ = rad.load_data(case='tag/', scenario='switch5steps2m')
+# cal_data, rec_data, _ = rad.load_data(case='tag/', scenario='on2m')
+# data = rad.calibration(cal_data, rec_data, 0)
+data = rec_data
+print(data.shape)
 
 rp = rad.compute_tof(data)
 
@@ -32,25 +36,39 @@ def get_vis_labels(arr, label_cnt=3, precision=0):
     vis = list(map(lambda x: ('%.' + str(precision) + 'f') % x, arr[vis_idx]))
     return vis_idx, vis
 
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(1, 2)
 plt.subplots_adjust(left=0.25, bottom=0.25)
-ax.set_xlabel("Range (m)")
+ax1.set_xlabel("Range (m)")
 
-rp_vis_idx, rp_vis = get_vis_labels(np.arange(Nfft) * c / (2 * Nfft * (freq[1] - freq[0])), label_cnt=5, precision=1)
-ax.set_xticks(rp_vis_idx, labels=rp_vis)
-rp_plot, = ax.plot(np.abs(rp[0]))
+vRange = np.arange(Nfft) * c / (2 * Nfft * (freq[1] - freq[0]))
+rp_vis_idx, rp_vis = get_vis_labels(vRange, label_cnt=5, precision=1)
+ax1.set_xticks(rp_vis_idx, labels=rp_vis)
+rp_plot_per_frame, = ax1.plot(np.abs(rp[0]))
+rp_plot_per_range, = ax2.plot(np.abs(rp[:, 0]))
 
 frame_slider_ax = plt.axes([0.25, 0.1, 0.65, 0.03])
 frame_slider = Slider(frame_slider_ax, 'Frame', 0, 99, valinit=0, valstep=1)
+range_bin_slider_ax = plt.axes([0.25, 0.06, 0.65, 0.03])
+range_bin_slider = Slider(range_bin_slider_ax, 'Range Bin', 0, Nfft-1, valinit=0, valstep=1)
 
-def localize(val):
+def rp_per_frame(val):
     frame_idx = frame_slider.val
 
     rp_single_frame = rp[frame_idx]
-    rp_plot.set_ydata(rp_single_frame)
+    rp_plot_per_frame.set_ydata(rp_single_frame)
     fig.canvas.draw_idle()
-    ax.set_ylim(np.min(rp_single_frame), np.max(rp_single_frame))
+    # ax1.set_ylim(np.min(rp_single_frame), np.max(rp_single_frame))
 
-frame_slider.on_changed(localize)
+def rp_per_range(val):
+    range_idx = range_bin_slider.val
+
+    rp_single_range = rp[:, range_idx]
+    rp_plot_per_range.set_ydata(rp_single_range)
+    fig.canvas.draw_idle()
+    ax2.set_ylim(np.min(rp_single_range), np.max(rp_single_range))
+    ax2.set_title(f'Range: {vRange[range_idx]}m')
+
+frame_slider.on_changed(rp_per_frame)
+range_bin_slider.on_changed(rp_per_range)
 
 plt.show()

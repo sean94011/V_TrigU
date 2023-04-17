1a6b170ddba90166c6851c0f8420c8df55e64c99
Sean Yao <sean94011@gmail.com>
2023-04-17 16:20:44 -0500

    update vital sign integration to point cloud

--- python/point_cloud_vital_sign.py
+++ python/point_cloud_vital_sign.py
@@ -11,8 +11,8 @@ from time import time
 
 start = time()
 # Load Data
-current_case = 'test01242023'
-current_scenario = 'cf_x_angle_+20'
+current_case = 'test04102023'
+current_scenario = 'human_longer'
 my_vtrig = isens_vtrigU(case=current_case)
 calArr, recArr = my_vtrig.load_data(case=current_case, scenario=current_scenario)
 
@@ -52,7 +52,10 @@ plt.show()
 # Generate the 3D map
 tof = np.fft.ifft(proArr,n=Nfft,axis=2)                      # Do IFFT across frequency steps to get the range bins
 tof[:,:,np.where(my_vtrig.dist_vec>bound)] = np.min(tof)       # Eliminate noise that is beyond the range
-tof = tof.reshape(nframe,20,20,-1)[chosen_frame,:,:,:]      # Reshape the range profile to Tx x Rx and extract a frame
+
+# tof = tof.reshape(nframe,20,20,-1)[chosen_frame,:,:,:]      # Reshape the range profile to Tx x Rx and extract a frame
+tof = tof.reshape(nframe,20,20,-1)
+tof = np.transpose(tof,axes=(1,2,3,0))
 # tof = np.linalg.norm(tof,axis=0)
 tof = np.fft.fft(tof,n=Nfft,axis=1)
 tof = np.fft.fft(tof,n=Nfft,axis=0)
@@ -60,11 +63,10 @@ tof = my_vtrig.normalization(tof)
 # tof[:,:,top_range_peaks] *= 10
 tof = np.roll(tof,shift=y_offset_shift,axis=1)
 tof = np.roll(tof,shift=x_offset_shift,axis=0)
-tof = np.abs(tof)
 # new shape: (AoD, AoA, Range) = (x, y, z)
 
 # Example 3D matrix (replace this with your matrix)
-matrix = tof
+matrix = np.abs(tof[:,:,:,chosen_frame])
 
 # Arrays to change the axis values (replace these with your arrays)
 x_array = (my_vtrig.angle_vec-90)*x_ratio
@@ -85,6 +87,29 @@ indices_3d = np.unravel_index(flat_indices, matrix.shape)
 
 # Extract x, y, and z coordinates of the top 6 peaks
 x_peaks, y_peaks, z_peaks = indices_3d
+doppler = tof.copy()
+doppler[x_peaks, y_peaks, z_peaks, :] *= 100
+doppler = my_vtrig.normalization(doppler)
+doppler = np.fft.fft(np.real(doppler), axis=3)
+d=(2*60+2.8)/500
+doppler_freq = np.fft.fftfreq(500,d)
+freq_low = 10
+freq_high = 230
+range_low = 100
+range_high = 350
+doppler = doppler[freq_low:freq_high,range_low:range_high]
+print(doppler.shape)
+"""
+plt.figure(figsize=(8,6))
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Range [m]')
+extent=[doppler_freq[freq_low],doppler_freq[freq_high],my_vtrig.dist_vec[range_low],my_vtrig.dist_vec[range_high]]
+plt.imshow(np.abs(doppler).T,origin='lower',aspect='auto',extent=extent)
+plt.colorbar()
+plt.show()
+
+
+print(matrix.shape)
 print(x_peaks)
 print(y_peaks)
 print(z_peaks)
@@ -98,11 +123,8 @@ ax = fig.add_subplot(111, projection='3d')
 # Get x, y, z indices for the entire matrix
 x, y, z = np.indices(matrix.shape)
 
-# Create a colormap to map the normalized values to colors
-# colormap = plt.cm.viridis
-
 # Add the entire matrix as blue points with changed axis values
-# ax.scatter(x_array[x], y_array[y], z_array[z], c=colormap(matrix.flatten()), alpha=0.6, label='All points')
+# ax.scatter(x_array[x], y_array[y], z_array[z], c='b', alpha=0.3, label='Original matrix')
 
 # Add the top 6 peaks to the plot with changed axis values
 ax.scatter(x_array[x_peaks], y_array[y_peaks], z_array[z_peaks], c='r', marker='x', s=100, label=f'Top {ntarget} peaks')
@@ -125,9 +147,8 @@ ax.legend()
 
 end = time()
 print(end-start, '[s]')
-plt.savefig(f'3D_Point_Cloud_{ntarget}_targets.png')
 
 # Show the plot
 plt.show()
 
-plt.savefig(f'3D_Point_Cloud_{ntarget}_targets_noPeak.png')
+"""

679d0b2854415279af5496a89840539df6dd165a
Power-tile <hejizheng2004@163.com>
2023-06-15 12:12:01 -0500

    Screen modulation finalized

---
+++ python/tag/screen/screen_const.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("200x200")
+window.configure(bg="black")
+
+window.mainloop()
--- python/tag/screen/screen_modulation.py
+++ python/tag/screen/screen_modulation.py
@@ -1,14 +1,18 @@
 import tkinter as tk
 
+color = "white"
+freq = 10 # Hz
+switching_time = int(1 / freq / 2 * 1000)
+
 def switch_color():
     current_color = window.cget("bg")
-    new_color = "black" if current_color == "red" else "red"
+    new_color = "black" if current_color == color else color
     window.configure(bg=new_color)
-    window.after(1000, switch_color)
+    window.after(switching_time, switch_color)
 
 window = tk.Tk()
 window.geometry("200x200")
-window.configure(bg="red")
+window.configure(bg=color)
 
 switch_color()
 
